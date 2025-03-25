0d137e64afa98df105fea8949679ede2ef38f776
ponomarev01 <74465644+ponomarev01@users.noreply.github.com>
2025-03-26 00:28:16 +0300

    Update AppDelegate.swift

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -1,1 +1,15 @@
+import UIKit
+import VK_ios_sdk
 
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        VKSdk.initialize(withAppId: "53326088") // Замените на ID из VK Dev
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        VKSdk.processOpen(url, fromApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String)
+        return true
+    }
+}
