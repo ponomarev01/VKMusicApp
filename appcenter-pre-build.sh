9e10a2766ac203584acfa5d71556d47eaadf848c
ponomarev01 <74465644+ponomarev01@users.noreply.github.com>
2025-03-26 03:12:58 +0300

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+echo "=== CLEANING PODS ==="
+rm -rf Pods/ Podfile.lock VKMusicApp.xcworkspace
+rm -rf ~/Library/Caches/CocoaPods
+
+echo "=== POD INSTALL ==="
+pod install --repo-update --verbose
+#!/usr/bin/env bash
 echo "=== Running Pre-build Script ==="
 rm -rf Pods/
 pod install --repo-update
 chmod +x appcenter-pre-build.sh
+
